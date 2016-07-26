b34b8780a3d8c300ce6b964a3e7e31ad5e52f929
Tim Feng <tim.feng@vpon.com>
2016-07-26 17:04:38 +0800

    Add quick reply

--- app/bot/jukebox.rb
+++ app/bot/jukebox.rb
@@ -7,37 +7,36 @@ Bot.on :message do |message|
   message.text
 
   Rails.logger.info { "[Process Begins] #{Time.now}" }
-  if message.text
-    result = MatchTagService.new.match(message.sender["id"], message.text.downcase)
-   # if matched, then return bot message, or return nothing
-    begin
-      if result
-        Bot.deliver(
-          recipient: message.sender,
-          message: result
-        )
-      else
-      end
-    rescue => e
+  if message.messaging["message"]["quick_reply"]
+    exact_message = message.messaging["message"]["quick_reply"]["payload"]
+  elsif message.text
+    exact_message = message.text.downcase
+  end
+  result = MatchTagService.new.match(message.sender["id"], exact_message)
+ # if matched, then return bot message, or return nothing
+  begin
+    if result
+      Bot.deliver(
+        recipient: message.sender,
+        message: result
+      )
+    else
     end
+  rescue => e
   end
   Rails.logger.info { "[Process Ends] #{Time.now}" }
 
 end
-
 Bot.on :postback do |postback|
   postback.sender    # => { 'id' => '1008372609250235' }
   postback.recipient # => { 'id' => '2015573629214912' }
   postback.sent_at   # => 2016-04-22 21:30:36 +0200
   postback.payload   # => 'EXTERMINATE'
-
   if postback.payload.include? 'CF_TAGS_OF_'
-    message_with_tags = MatchTagService.new.find_tags(postback.payload)
+    result = MatchTagService.new.find_tags(postback.payload)
     Bot.deliver(
       recipient: postback.sender,
-      message: {
-        text: message_with_tags
-      }
+      message: result
     )
   end
 end
