88bcf572e1287a748098eb5cf835812922209e73
Tim Feng <tim.feng@vpon.com>
2016-08-08 17:17:15 +0800

    add tag no.27

--- app/services/match_tag_service.rb
+++ app/services/match_tag_service.rb
@@ -14,7 +14,7 @@ class MatchTagService
         greeting
       when "m"
         reply_jukebox_guide
-      when "m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9", "m10", "m11", "m12", "m13", "m14", "m15", "m16", "m17", "m18", "m19", "m20", "m21", "m22", "m23", "m24", "m25", "m26"
+      when "m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9", "m10", "m11", "m12", "m13", "m14", "m15", "m16", "m17", "m18", "m19", "m20", "m21", "m22", "m23", "m24", "m25", "m26", "m27"
         register_user
         increase_tag_searched_count
         increate_user_searched_tag_and_count
